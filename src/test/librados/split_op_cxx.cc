3776c1137017e73f26d5273ff8b4e4fcf34de5be
Callum James <callum.james@ibm.com>
2025-11-10 17:30:01 +0000

    Formatting

--- src/test/librados/split_op_cxx.cc
+++ src/test/librados/split_op_cxx.cc
@@ -12,6 +12,35 @@ using namespace librados;
 typedef RadosTestPP LibRadosSplitOpPP;
 typedef RadosTestECPP LibRadosSplitOpECPP;
 
+TEST_P(LibRadosSplitOpECPP, XattrReads) {
+  SKIP_IF_CRIMSON();
+  bufferlist bl, attr_bl, attr_read_bl;
+  std::string attr_key = "my_key";
+  std::string attr_value = "my_attr";
+
+  bl.append("ceph");
+  ObjectWriteOperation write1;
+  write1.write(0, bl);
+  encode(attr_value, attr_bl);
+  write1.setxattr(attr_key.c_str(), attr_bl);
+  ASSERT_TRUE(AssertOperateWithoutSplitOp(0, "foo", &write1));
+
+  ObjectReadOperation read;
+  read.read(0, bl.length(), NULL, NULL);
+
+  int getxattr_rval, getxattrs_rval;
+  read.getxattr(attr_key.c_str(), &attr_read_bl, &getxattr_rval);
+  std::map<string, bufferlist> pattrs{ {"_", {}}, {attr_key, {}}};
+  read.getxattrs(&pattrs, &getxattrs_rval);
+  read.cmpxattr(attr_key.c_str(), CEPH_OSD_CMPXATTR_OP_EQ, attr_bl);
+
+  ASSERT_TRUE(AssertOperateWithSplitOp(1, "foo", &read, &bl));
+  ASSERT_EQ(0, memcmp(bl.c_str(), "ceph", 4));
+
+  ASSERT_EQ(0, getxattr_rval);
+  ASSERT_EQ(0, getxattrs_rval);
+}
+
 TEST_P(LibRadosSplitOpECPP, OMAPReads) {
   SKIP_IF_CRIMSON();
   bufferlist bl_write, omap_val_bl, omap_header_bl;
@@ -162,35 +191,6 @@ TEST_P(LibRadosSplitOpECPP, OMAPReads) {
   ASSERT_EQ(returned_keys.size(), (unsigned)0);
 
   std::cout << "--- OMap Clear testing passed ---" << std::endl;
-}
-
-TEST_P(LibRadosSplitOpECPP, XattrReads) {
-  SKIP_IF_CRIMSON();
-  bufferlist bl, attr_bl, attr_read_bl;
-  std::string attr_key = "my_key";
-  std::string attr_value = "my_attr";
-
-  bl.append("ceph");
-  ObjectWriteOperation write1;
-  write1.write(0, bl);
-  encode(attr_value, attr_bl);
-  write1.setxattr(attr_key.c_str(), attr_bl);
-  ASSERT_TRUE(AssertOperateWithoutSplitOp(0, "foo", &write1));
-
-  ObjectReadOperation read;
-  read.read(0, bl.length(), NULL, NULL);
-
-  int getxattr_rval, getxattrs_rval;
-  read.getxattr(attr_key.c_str(), &attr_read_bl, &getxattr_rval);
-  std::map<string, bufferlist> pattrs{ {"_", {}}, {attr_key, {}}};
-  read.getxattrs(&pattrs, &getxattrs_rval);
-  read.cmpxattr(attr_key.c_str(), CEPH_OSD_CMPXATTR_OP_EQ, attr_bl);
-
-  ASSERT_TRUE(AssertOperateWithSplitOp(1, "foo", &read, &bl));
-  ASSERT_EQ(0, memcmp(bl.c_str(), "ceph", 4));
-
-  ASSERT_EQ(0, getxattr_rval);
-  ASSERT_EQ(0, getxattrs_rval);
 }
 
 INSTANTIATE_TEST_SUITE_P_EC(LibRadosSplitOpECPP);
