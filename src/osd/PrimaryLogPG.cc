ad56f4b88e7c029b5378299aced1d3baed262c0c
Matty Williams <Matty.Williams@ibm.com>
2026-05-01 12:07:36 +0100

    osd: Adjust lock type selection in PrimaryLogPG::get_rw_locks
    All writes in EC are asynchronous (even if they are only updating metadata), so metadata reads in EC must use the RWEXCL lock to eliminate the possibility of race conditions.
    This resolves failures with Cls queue tests on EC pools where read operations were observed overtaking in-flight writes.
    
    Fixes: https://tracker.ceph.com/issues/74531
    Signed-off-by: Matty Williams <Matty.Williams@ibm.com>

--- src/osd/PrimaryLogPG.cc
+++ src/osd/PrimaryLogPG.cc
@@ -1697,7 +1697,8 @@ bool PrimaryLogPG::get_rw_locks(bool write_ordered, OpContext *ctx)
    * to get the second.
    */
   if (write_ordered && ctx->op->may_read()) {
-    if (ctx->op->may_read_data()) {
+    // In EC, reads can overtake writes unless the RWEXCL lock is held
+    if (ctx->op->may_read_data() || pool.info.is_erasure()) {
       ctx->lock_type = RWState::RWEXCL;
     } else {
       ctx->lock_type = RWState::RWWRITE;
