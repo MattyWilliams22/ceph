2abcc25bb61332d0595aa0822ebfe42607606206
Matty Williams <Matty.Williams@ibm.com>
2026-05-20 17:05:35 +0100

    test: Improve speed of omap tests
    Use a single pool of each type for all tests instead of creating a new pool for each test.
    
    Signed-off-by: Matty Williams <Matty.Williams@ibm.com>

--- src/test/librados/test_pool_types.h
+++ src/test/librados/test_pool_types.h
@@ -3,7 +3,11 @@
 
 #pragma once
 
+#include <chrono>
+#include <map>
 #include <string>
+#include <vector>
+
 #include "include/rados/librados.hpp"
 #include "test/librados/test_cxx.h"
 #include "gtest/gtest.h"
@@ -39,9 +43,9 @@ inline std::string create_pool_by_type(
     PoolType type) {
   switch (type) {
     case PoolType::REPLICATED:
-      return create_one_pool_pp(pool_name, cluster);
+      return create_pool_pp(pool_name, cluster);
     case PoolType::FAST_EC: {
-      std::string result = create_one_ec_pool_pp(pool_name, cluster, true, true);
+      std::string result = create_ec_pool_pp(pool_name, cluster, true, true);
       if (result != "") {
         return result;
       }
@@ -50,7 +54,7 @@ inline std::string create_pool_by_type(
       return result;
     }
     case PoolType::LEGACY_EC:
-      return create_one_ec_pool_pp(pool_name, cluster, false, false);
+      return create_ec_pool_pp(pool_name, cluster, false, false);
     default:
       return "Unknown pool type";
   }
@@ -63,10 +67,10 @@ inline int destroy_pool_by_type(
     PoolType type) {
   switch (type) {
     case PoolType::REPLICATED:
-      return destroy_one_pool_pp(pool_name, cluster);
+      return destroy_pool_pp(pool_name, cluster);
     case PoolType::FAST_EC:
     case PoolType::LEGACY_EC:
-      return destroy_one_ec_pool_pp(pool_name, cluster);
+      return destroy_ec_pool_pp(pool_name, cluster);
     default:
       return -EINVAL;
   }
@@ -77,20 +81,99 @@ inline int destroy_pool_by_type(
 class PoolTypeTestFixture : public ::testing::TestWithParam<PoolType> {
  protected:
   static librados::Rados rados;
+  static std::map<PoolType, std::string> pool_names;
   librados::IoCtx ioctx;
   std::string pool_name;
+  std::string nspace;
   PoolType pool_type;
+
+  static std::vector<PoolType> get_supported_pool_types() {
+    return {PoolType::REPLICATED, PoolType::FAST_EC};
+  }
+
+  static std::string pool_name_prefix() {
+    return "pool_type_test_";
+  }
+
+  static void after_pool_create(PoolType type,
+                                const std::string& pool_name,
+                                librados::Rados& cluster) {
+  }
+
+  static void cleanup_namespace(librados::Rados& cluster,
+                                librados::IoCtx& ioctx,
+                                const std::string& ns) {
+    ioctx.snap_set_read(librados::SNAP_HEAD);
+    ioctx.set_namespace(ns);
+
+    int tries = 20;
+    while (--tries) {
+      int got_enoent = 0;
+      for (librados::NObjectIterator it = ioctx.nobjects_begin();
+           it != ioctx.nobjects_end(); ++it) {
+        ioctx.locator_set_key(it->get_locator());
+        librados::ObjectWriteOperation op;
+        op.remove();
+        librados::AioCompletion* completion = cluster.aio_create_completion();
+        int ret = ioctx.aio_operate(it->get_oid(), completion, &op,
+                                    librados::OPERATION_IGNORE_CACHE);
+        if (ret == 0) {
+          completion->wait_for_complete();
+          if (completion->get_return_value() == -ENOENT) {
+            ++got_enoent;
+          } else {
+            ASSERT_EQ(0, completion->get_return_value());
+          }
+        }
+        completion->release();
+      }
+      if (!got_enoent) {
+        break;
+      }
+      sleep(1);
+    }
+  }
+
+  static void SetUpTestSuite() {
+    ASSERT_EQ("", connect_cluster_pp(rados));
+
+    for (auto type : get_supported_pool_types()) {
+      std::string pname = get_temp_pool_name(
+        pool_name_prefix() + pool_type_name(type) + "_");
+      ASSERT_EQ("", create_pool_by_type(pname, rados, type));
+      after_pool_create(type, pname, rados);
+      pool_names[type] = pname;
+    }
+  }
+
+  static void TearDownTestSuite() {
+    for (auto& [type, pname] : pool_names) {
+      ASSERT_EQ(0, destroy_pool_by_type(pname, rados, type));
+    }
+    pool_names.clear();
+    rados.shutdown();
+  }
 
   void SetUp() override {
     pool_type = GetParam();
-    pool_name = get_temp_pool_name();
-    ASSERT_EQ("", create_pool_by_type(pool_name, rados, pool_type));
+    auto it = pool_names.find(pool_type);
+    ASSERT_NE(pool_names.end(), it);
+    pool_name = it->second;
     ASSERT_EQ(0, rados.ioctx_create(pool_name.c_str(), ioctx));
+
+    auto test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+    nspace = std::string(test_info->name()) + "_" +
+             std::to_string(std::chrono::system_clock::now()
+                              .time_since_epoch().count());
+    ioctx.set_namespace(nspace);
+
+    cleanup_namespace(rados, ioctx, nspace);
   }
-  
+
   void TearDown() override {
+    cleanup_namespace(rados, ioctx, "");
+    cleanup_namespace(rados, ioctx, nspace);
     ioctx.close();
-    ASSERT_EQ(0, destroy_pool_by_type(pool_name, rados, pool_type));
   }
 };
 
